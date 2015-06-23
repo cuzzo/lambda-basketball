590aa850fa6fc4f6d12bc57296f136b3e294666e
Cuzzo <yahn007@outlook.com>
2015-06-22 23:13:17 -0700

    Add collapsable sections.

---
+++ shrink.js
@@ -0,0 +1,19 @@
+(function() {
+  "use strict";
+
+  document.addEventListener("DOMContentLoaded", function(ev) {
+    var mobile_rgx = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i,
+        sections = document.querySelectorAll(".section--separated"),
+        i;
+
+    for (i = 0; i < sections.length; i++) {
+      sections[i].addEventListener("click", function(ev) {
+          this.classList.toggle("section--collapsed");
+        });
+
+      if (!mobile_rgx.test(navigator.agent)) continue;
+      sections[i].classList.add("section--collapsed");
+    }
+  });
+
+})();
